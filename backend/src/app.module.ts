d930611046e8cfe23f5d7b86478abaf7658e67a8
Kanok <supani2521@gmail.com>
2026-03-28 01:22:04 +0700

    test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,13 +4,12 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import { ConfigModule } from '@nestjs/config';
-
+// import { ConfigModule } from '@nestjs/config';
+// ConfigModule.forRoot({
+//       isGlobal: true,
+//     })
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),PrismaModule, AuthModule, ReviewsModule],
+  imports: [PrismaModule, AuthModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
